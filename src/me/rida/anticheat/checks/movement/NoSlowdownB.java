7e33d14cc8e65d43dd8bf60b474f95aa80c2cfe1
ridalarry <rida.larry@live.com>
2018-12-02 03:12:50 +0300

    Update NoSlowdownB.java
    More debuging and commented the notifications and warnings to not do anything even if enabled

--- src/me/rida/anticheat/checks/movement/NoSlowdownB.java
+++ src/me/rida/anticheat/checks/movement/NoSlowdownB.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -26,31 +27,35 @@ public class NoSlowdownB extends Check {
 
 	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGH)
 	private void onMove(PlayerMoveEvent e) {
-		if (e.getTo().getX() == e.getFrom().getX() && e.getFrom().getY() == e.getTo().getY()
-				&& e.getTo().getZ() == e.getFrom().getZ()) {
-			return;
-		}
-		Player p = e.getPlayer();
-		double OffsetY = MathUtil.offset(MathUtil.getVerticalVector(e.getFrom().toVector()),
-				MathUtil.getVerticalVector(e.getTo().toVector()));
-		//double Offset = e.getFrom().getY() - e.getFrom().getY();
-		//double Offset2 = e.getFrom().getY() - e.getFrom().getY();
-		//double OffsetXZ = MathUtil.offset(MathUtil.getHorizontalVector(e.getFrom().toVector()),
-				//MathUtil.getHorizontalVector(e.getTo().toVector()));
+		{
+			Location from = e.getFrom();
+			Location to = e.getTo();
+			if (to.getX() == e.getFrom().getX() && from.getY() == to.getY()
+					&& from.getZ() == from.getZ()) {
+				return;
+			}
+			Player p = e.getPlayer();
 
-		if (!BlockUtil.isNearLiquid(p)
-				|| p.getAllowFlight() 
-				|| p.getGameMode().equals(GameMode.CREATIVE)
-				|| PlayerUtil.isNearSlab(p)
-				|| BlockUtil.isNearFence(p)
-				|| BlockUtil.isNearStair(p)
-				|| PlayerUtil.isNearSign(p)
-				//|| e.getFrom().getY() > e.getTo().getY()
-				//|| e.getFrom().getY() < e.getTo().getY()
-				|| OffsetY < 0.13 ) {
-			return;
+			double OffsetY = MathUtil.offset(MathUtil.getVerticalVector(from.toVector()), MathUtil.getVerticalVector(to.toVector()));
+			double Offset = e.getFrom().getY() - e.getFrom().getY();
+			double Offset2 = e.getFrom().getY() - e.getFrom().getY();
+			double OffsetXZ = MathUtil.offset(MathUtil.getHorizontalVector(from.toVector()), MathUtil.getHorizontalVector(to.toVector()));
+
+			if (!BlockUtil.isNearLiquid(p)
+					|| p.getAllowFlight() 
+					|| p.getGameMode().equals(GameMode.CREATIVE)
+					|| PlayerUtil.isNearSlab(p)
+					|| BlockUtil.isNearFence(p)
+					|| BlockUtil.isNearStair(p)
+					|| PlayerUtil.isNearSign(p)
+					//|| from.getY() > to.getY()
+					//|| from.getY() < to.getY()
+					|| OffsetY < 0.13 ) {
+				return;
+			}
+			if (PlayerUtil.isInLiquid(p))
+				//getAntiCheat().logCheat(this, p, "OffsetY: " + OffsetY + " OffsetXZ: " + OffsetXZ + " Offset: " + Offset + " Offset2: " + Offset2 + " From Y: " + from.getY() + " To Y:  " + to.getY(), "(Type: B)")
+				;
 		}
-		if (PlayerUtil.isInLiquid(p))
-			getAntiCheat().logCheat(this, p, "Offset: " + OffsetY + " From Y: " + e.getFrom().getY() + " To Y:  " + e.getTo().getY(), "(Type: A)");
 	}
 }
