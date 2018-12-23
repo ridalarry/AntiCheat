05d203667b3d6f55f183c4fa283998e065b83754
Rida <rida.larry@gmail.com>
2018-12-23 05:43:32 -0800

    Update ReachA.java

--- src/me/rida/anticheat/checks/combat/ReachA.java
+++ src/me/rida/anticheat/checks/combat/ReachA.java
@@ -60,7 +60,7 @@ public class ReachA extends Check {
 		double KB = getKB(p);
 		maxReach+= Math.abs(p.getVelocity().length() + p2.getVelocity().length()) * 0.4;
 		maxReach+= yawDifference * 0.01;
-		maxReach+= getAntiCheat().getLag().getPing(p) * 0.00097; 
+		maxReach+= getAntiCheat().getLag().getPing(p) * 0.01097; 
 
 		if(maxReach < 4.2) {
 			maxReach = 4.2;
